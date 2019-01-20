04a12303a9a43b4dc2d5aed40a6969997d73550d
Ahmed Farahat <ahmedmfarahat@users.noreply.github.com>
2019-01-20 12:52:35 +0200

    add default back

--- processing/analysis/analysis.js
+++ processing/analysis/analysis.js
@@ -244,7 +244,7 @@ function analyzeImage(args, fileName, analyzeCallback) {
           method: 'POST',
           url: 'https://gateway.watsonplatform.net/visual-recognition/api/v3/classify' + // eslint-disable-line
            '?api_key=' + args.watsonApiKey +
-           '&classifier_ids=DefaultCustomModel_1626596963' +
+           '&classifier_ids=default,DefaultCustomModel_1626596963' +
 '&version=2018-03-19',
           auth: {
             user: 'apikey',
